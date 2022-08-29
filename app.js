c880d8c2506625bf2c344630257186dbb9eca955
DevelopJKong <ybaram40@gmail.com>
2022-08-29 21:31:58 +0900

    #2.2 Line Width 완료

--- app.js
+++ app.js
@@ -1,47 +1,37 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 800;
-canvas.height = 800;
+canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
+canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
+ctx.lineWidth = lineWidth.value;
 
-ctx.fillRect(210 - 40 , 200 - 10, 15, 100);
-ctx.fillRect(360 - 40 , 200 - 10, 15, 100);
-ctx.fillRect(260 - 40 , 200 - 10, 60, 200);
-ctx.arc(250, 100, 50, 0, 2 * Math.PI);
-ctx.fill();
+let isPainting = false;
 
-ctx.beginPath();
-ctx.fillStyle = "red";
-ctx.arc(260,80,5,0,2 * Math.PI);
-ctx.arc(220,80,5,0,2 * Math.PI);
-ctx.fill();
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.beginPath();
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
 
+function startPainting() {
+  isPainting = true;
+}
+function cancelPainting() {
+  isPainting = false;
+}
 
-//지붕 만들기
-// ctx.fillRect(200, 200, 50, 200);
-// ctx.fillRect(400, 200, 50, 200);
-// ctx.lineWidth = 2;
-// ctx.strokeRect(300, 300, 50, 100);
-// ctx.fillRect(200, 200, 200, 20);
-// ctx.moveTo(200, 200);
-// ctx.lineTo(325, 100);
-// ctx.lineTo(450, 200);
-// ctx.stroke();
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
 
-//// ctx.moveTo(50, 50);
-//// ctx.lineTo(100, 50);
-//// ctx.lineTo(100, 100);
-//// ctx.lineTo(50, 100);
-//// ctx.lineTo(50, 50);
-//// ctx.stroke();
-////
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
 
-// // ctx.rect(50,50,100,100);
-// // ctx.rect(150,150,100,100);
-// // ctx.rect(250,250,100,100);
-// // ctx.fill();
-// //
-// // ctx.beginPath();
-// // ctx.rect(350,350,100,100);
-// // ctx.fillStyle = "red";
-// // ctx.fill();
+lineWidth.addEventListener("change", onLineWidthChange);
